9bdf611550b00ebce4230738785cbe0f926d9d84
Alina <miskova007g@gmail.com>
2026-06-01 20:14:58 +0700

    Add files via upload

---
+++ Probnik.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int variable; // переменная для проверки
+
+    // Пример: присвоим значение или получим из ввода
+    // cin >> variable;
+
+    switch (variable) {
+    case 1:
+        // действие для значения 1
+        break;
+    case 2:
+        // действие для значения 2
+        break;
+        // добавьте нужные case
+    default:
+        // действие по умолчанию
+        break;
+    }
+
+    return 0;
+}
